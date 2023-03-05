8633df8875107650b49c87d871e2e5898988c3ff
Willian Marioti <willian.marioti@gmail.com>
2023-03-05 13:21:51 -0300

    Add project files.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int escolha, numero, res;
+
+        Console.WriteLine("Escolha a tabuada de 1 a 9: ");
+        escolha = int.Parse(Console.ReadLine());
+
+        for(numero=0; numero<11; numero++)
+        {
+            res = escolha * numero;
+            Console.WriteLine("\n {0} x {1} = {2}", escolha, numero, res);
+        }
+    }
+}
